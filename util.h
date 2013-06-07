b69b7cddc29b332311d443594f3c9e08d8d0bc6d
Jay Yang <jkelleyy@gmail.com>
2013-06-07 16:13:44 -0500

    add a flag to turn on and of debugging

--- util.h
+++ util.h
@@ -4,6 +4,7 @@
 #include <cstdio>
 using namespace std;
 
+#ifdef DEBUG_OUTPT_ON
 //various utilities
 //TODO add flags to turn these on and off
 #define TRACE(...) fprintf(stderr,__VA_ARGS__)
@@ -11,6 +12,16 @@ using namespace std;
 #define WARN(...) fprintf(stderr,__VA_ARGS__)
 #define DEBUG(...) fprintf(stderr,__VA_ARGS__)
 #define INFO(...) fprintf(stderr,__VA_ARGS__)
+
+#else
+#define TRACE(...) do{}while(0)
+#define ERROR(...) do{}while(0)
+#define WARN(...) do{}while(0)
+#define DEBUG(...) do{}while(0)
+#define INFO(...) do{}while(0)
+
+#endif
+
 
 //stuff that should probably go elsewhere
 #define NEG_INF -100000000
